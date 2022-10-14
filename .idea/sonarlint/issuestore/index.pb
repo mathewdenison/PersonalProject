411be3ab292bab1559b105d8e82c1eec324f8012
Mathew <mathewdenison@gmail.com>
2022-10-14 17:32:05 -0400

    First Commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+D
+Personal Project.iml,6\6\66cbf488e0d6e96b279a87500ed7dd2b93da7c8a
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    static void arrays() {
+        System.out.println("\nInside arrays ...");
+        int[] scores = new int[4];
+        scores[0] = 90;
+        scores[1] = 70;
+        scores[2] = 80;
+        scores[3] = 100;
+
+        System.out.println("In index 1: " + scores[0]);
+        System.out.println("In index 2: " + scores[1]);
+        System.out.println("In index 3: " + scores[2]);
+        System.out.println("In index 4: " + scores[3]);
+
+    }
+    public static void main(String[] args) {
+        arrays();
+    }
+}
+
